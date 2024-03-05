529430f97fb56eadda9fc2d5595cfe4956148f43
jeremiahb <jbthelegend15@gmail.com>
2024-03-04 23:39:24 -0600

    set up push to state array. AddEmployee component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import AddEmployee from './components/AddEmployee';
 
 function App() {
   const [role, setRole] = useState('devs');
@@ -54,6 +55,16 @@ function App() {
     setEmployees(updatedEmployees);
   }
 
+  function newEmployee(name, role, img) {
+    const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img,
+    };
+    setEmployees([...employees, newEmployee]);
+  }
+
   const showEmployees = true;
   return (
     <div className="App">
@@ -79,6 +90,7 @@ function App() {
               );
             })}
           </div>
+          <AddEmployee newEmployee={newEmployee}/>
         </>
       ) : (
         <p>You cannot see the employees</p>
